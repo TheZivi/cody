5a32d13d52b4f9d51aa1277f62c4d281cce969d8
TheZivi <lostintimesofold@gmail.com>
2025-06-29 22:54:08 -0500

    Add files via upload

--- language.php
+++ language.php
@@ -1,4 +1,8 @@
-<?php
-require('../config.php');
-echo introLanguage();
-?>
+<div onclick="loadLanguage('<?php echo $boom; ?>');" class="list_element btable lang_box">
+	<div class="bcell_mid lang_flag_box">
+		<img alt="<?php echo $boom; ?> flag" class="lang_flag" src="system/language/<?php echo $boom; ?>/flag.png"/> 
+	</div>
+	<div class="bcell_mid lang_lang">
+		<?php echo $boom; ?>
+	</div>
+</div>
--- news.php
+++ news.php
@@ -1,69 +1,50 @@
-<?php
-/**
-* Codychat
-*
-* @package Codychat
-* @author www.boomcoding.com
-* @copyright 2020
-* @terms any use of this script without a legal license is prohibited
-* all the content of Codychat is the propriety of BoomCoding and Cannot be 
-* used for another project.
-*/
-require_once('../config_session.php');
-
-$news_content = '';
-$news_count = 0;
-
-$get_news = $mysqli->query("SELECT boom_news.*, boom_users.*,
-(SELECT count(id) FROM boom_news) as news_count,
-(SELECT count( parent_id ) FROM boom_news_reply WHERE parent_id = boom_news.id ) as reply_count,
-(SELECT like_type FROM boom_news_like WHERE uid = '{$data['user_id']}' AND like_post = boom_news.id) as liked
-FROM boom_news, boom_users
-WHERE boom_news.news_poster = boom_users.user_id 
-ORDER BY news_date DESC LIMIT 10");
-
-if($get_news->num_rows > 0){
-	while ($news = $get_news->fetch_assoc()){
-		$news_count = $news['news_count'];
-		$news_content .= boomTemplate('element/news', $news);
-	}
-	$mysqli->query("UPDATE boom_users SET user_news = '" . time() . "' WHERE user_id = '{$data['user_id']}'");
-}
-else {
-	$news_content .= emptyZone($lang['no_news']);
-}
-?>
-<div class="boom_keep">
-	<div class="pad20">
-		<?php if(canPostNews()){ ?>
-		<div class="vpad10">
-			<div class="add_post_container">
-				<div id="add_wall_form">
-					<div class="post_input_container">
-						<textarea onkeyup="textArea(this, 60);" id="news_data" maxlength="3000" spellcheck="false" placeholder="<?php echo $lang['start_new_post']; ?>" class="full_textarea" ></textarea>
-						<div id="post_file_data" class="pad10 main_post_data hidden" data-key="">
-						</div>
-					</div>
-					<div class="main_post_control">
-						<div class="main_post_item">
-							<i class="fa fa-image"></i>
-							<input id="news_file" onchange="uploadNews();" type="file"/>
-						</div>
-						<div class="main_post_button">
-							<button onclick="sendNews();" class="small_button rounded_button theme_btn"><i class="fa fa-send"></i> <?php echo $lang['post']; ?></button>
-						</div>
-					</div>
+<div id="boom_news<?php echo $boom['id']; ?>" data="<?php echo $boom['id']; ?>" class="news_box post_element">
+	<div class="post_title">
+		<div class=" post_avatar get_info" data="<?php echo $boom['user_id']; ?>">
+			<img src="<?php echo myAvatar($boom['user_tumb']); ?>"/>
+		</div>
+		<div class="bcell_mid hpad5 maxflow post_info">
+			<p class="username text_small <?php echo myColor($boom); ?>"><?php echo $boom['user_name']; ?></p>
+			<p class="text_xsmall date"><?php echo displayDate($boom['news_date']); ?></p>
+		</div>
+		<div onclick="openPostOptions(this);" class="post_edit bcell_mid_center">
+			<i class="fa fa-ellipsis-h"></i>
+			<div class="post_menu fmenu">
+				<div onclick="viewNewsLikes(<?php echo $boom['id']; ?>);" class="fmenu_item fmenut">
+					<?php echo $lang['view_likes']; ?>
 				</div>
+				<?php if(canDeleteNews($boom) || mySelf($boom['news_poster'])){ ?>
+				<div onclick="openDeletePost('news', <?php echo $boom['id']; ?>);" class="fmenu_item fmenut">
+					<?php echo $lang['delete']; ?>
+				</div>
+				<?php } ?>
 			</div>
 		</div>
-		<?php } ?>
-		<div id="container_news">
-			<?php echo $news_content; ?>
+	</div>
+	<div class="post_content">
+		<?php echo boomPostIt($boom, $boom['news_message']); ?>
+		<?php echo boomPostFile($boom['news_file']); ?>
+	</div>
+	<div class="post_control btauto">
+		<div class="bcell_mid like_container newslike<?php echo $boom['id']; ?>">
+			<?php echo getLikes($boom['id'], $boom['liked'], 'news'); ?>
 		</div>
-		<?php if($news_count > 10){ ?>
-		<div class="load_more_news centered_element">
-			<button onclick="moreNews(this);" class="theme_btn small_button rounded_button load_more"><i class="fa fa-plus"></i> <?php echo $lang['load_more']; ?></button>
+		<div data="0" class="bcell_mid comment_count bcauto load_comment <?php if($boom['reply_count'] < 1){ echo 'hidden'; } ?>" onclick="loadNewsComment(this, <?php echo $boom['id']; ?>);">
+			<span id="nrepcount<?php echo $boom['id']; ?>"><?php echo $boom['reply_count']; ?></span> <img class="comment_icon" src="<?php echo $data['domain']; ?>/default_images/icons/comment.svg"/>
 		</div>
-		<?php } ?>
+	</div>
+	<?php if(!muted() && boomAllow($cody['can_reply_news'])){ ?>
+	<div class="add_comment_zone cmb<?php echo $boom['id']; ?>">
+		<div class="tpad10 reply_post">
+			<input onkeydown="newsReply(event, <?php echo $boom['id']; ?>, this);" maxlength="500" placeholder="<?php echo $lang['comment_here']; ?>" class="add_comment full_input">
+		</div>
+	</div>
+	<?php } ?>
+	<div class="tpad10 ncmtboxwrap<?php echo $boom['id']; ?>">
+		<div class="ncmtbox ncmtbox<?php echo $boom['id']; ?>">
+		</div>
+		<div class="nmorebox nmorebox<?php echo $boom['id']; ?>">
+		</div>
+		<div class="clear"></div>
 	</div>
 </div>
